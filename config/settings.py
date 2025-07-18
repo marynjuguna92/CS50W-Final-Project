ea0c545c308c1bb4dc04098b061622204c16256a
Mary Njuguna <marynjugunah23@gmail.com>
2025-07-18 21:02:55 +0300

    Add new home link and style success/error message in contact form

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
